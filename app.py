7bc719b0887511d141b2e363f640de083635331c
CHIJIUBA <chijiubaonyedikachukwu@gmail.com>
2024-10-23 10:56:36 +0100

    go

---
+++ app.py
@@ -0,0 +1,45 @@
+import json
+from flask import Flask, request, jsonify, session
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
+
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "snapnet_interview_test"
+jwt = JWTManager(app)
+
+
+@app.route('/login', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token": access_token}
+    return response, 200
+
+@app.route('/signup', methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    
+    if email != "test" or password != "test":
+        return {"msg": "Wrong email or password"}, 401
+
+    access_token = create_access_token(identity=email)
+    response = {"access_token": access_token}
+    return response, 200
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
